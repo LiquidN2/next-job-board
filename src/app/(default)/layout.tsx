dd24583633d68b25de98b1a53a435a0f1d3294c8
Hugh Nguyen <hhnguyen255@gmail.com>
2024-06-07 10:54:21 +0700

    Scaffolded default vs guest layout

---
+++ src/app/(default)/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Suspense } from 'react';
+// import Loading from './loading';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+// import Nav from '@/components/Nav';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Next Job Board',
+  description: 'A Web App by NextJS',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full bg-gray-100">
+      <body className={`h-full ${inter.className}`}>
+        <div className="min-h-full">
+          {/* <Nav /> */}
+          {/* <Suspense fallback={<Loading />}>{children}</Suspense> */}
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(default)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      {/* <Header>Dashboard</Header> */}
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+          Dashboard
+        </div>
+      </main>
+    </>
+  );
+}
